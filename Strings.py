1d8e7a387166b8fe33ce680bfd7a5c455da815b9
Shaurya144 <chundawatshaurya@gmail.com>
2024-07-13 17:18:58 +0100

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,17 @@
+# in Python "Potato" is the same as 'Potato'
+
+# You can use them inside one another as long as they don't intefere
+# Eg.
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+# You can assign a string to a variable
+StringVariable = "Potato"
+print(StringVariable)
+
+# For Multiline strings used Three quotes
+aString = """Hello how are you doing today? 
+Wonderful day isn't it? I agree, On weekends sometimes."""
+print(aString)
